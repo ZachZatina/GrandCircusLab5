88776618aca9756a0fd4c4bf2551c8ce18ef25c2
ZachZatina <zachzatina327@yahoo.com>
2018-01-25 19:13:26 -0500

    added comments

--- src/LabFive.java
+++ src/LabFive.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
 public class LabFive {
 
 	public static void main(String[] args) {
+		// Variable list
 		Scanner scnr = new Scanner(System.in);
 		int numSides;
 		char cont = 'y';
@@ -23,25 +24,33 @@ public class LabFive {
 		boolean a;
 		boolean outerTrue = true;
 		boolean b;
-
+		
+		// A loop to see if the user wants to continue rolling dice with a
+		//  different amount of sides.
 		while (outerTrue == true) {
+			//these need to be assigned here to allow while loops w/in code to run correctly
 			a = true;
 			b = true;
 			innerTrue = true;
+			// This is here to reset the counting of the rolls
 			i = 0;
+			// App introduction
 			System.out.println("Welcome to the Dice Rolling app!");
 			System.out.println("");
 			System.out.print("How many sides do the two dies have?: ");
-
+			
+			// Validates for integers
 			while (!scnr.hasNextInt()) {
 				System.out.println("");
 				System.out.println("Please enter a valid integer: ");
 				scnr.nextLine();
 			}
 			numSides = scnr.nextInt();
-
+			
+			// Loop to run actual dice rolling.
 			while (innerTrue == true) {
-
+				
+				// Output and input to see how many times the user wants to roll the dice
 				System.out.print("How many times would you like to roll the dice?: ");
 
 				while (!scnr.hasNextInt()) {
@@ -50,25 +59,32 @@ public class LabFive {
 					scnr.nextLine();
 				}
 				numRolls = scnr.nextInt();
-
+				
+				// Loop to match how many times the user wants to roll dice
 				for (int j = 1; j <= numRolls; j++) {
-
+					
+					// Part that calls the method that assigns a value to the "roll"
 					roll1 = getDiceRoll(numSides);
 					roll2 = getDiceRoll(numSides);
-
+					
+					//counting and reset of while loop value
 					i = i + 1;
 					a = true;
-
+					
+					// Output of results
 					System.out.println("Roll " + i + ":");
 					System.out.println(roll1);
 					System.out.println(roll2);
-
+					
+					// This is the part that calls the method to determine the phrase if
+					//  dice has 6 sides
 					if (numSides == 6) {
 						System.out.print(getPhrase(roll1, roll2));
 						System.out.println("");
 					}
 				}
-
+				
+				// Loop to  determine if the user wants to roll the same sided dice again
 				scnr.nextLine();
 				System.out.print("Would you like to continue with this amount of sides on the dice? (y/n): ");
 				while (a == true) {
@@ -87,7 +103,8 @@ public class LabFive {
 					}
 				}
 			}
-
+			
+			// Loop to see if user wants to change dice or exit program
 			System.out.print("Would you like to continue and change how many sides on the dice there are? (y/n): ");
 			while (b == true) {
 				cont = scnr.next().charAt(0);
@@ -106,16 +123,16 @@ public class LabFive {
 			}
 
 		}
-
+		// Closing statements
 		System.out.println("Thank you for using the Dice Rolling App! Goodbye!");
 		scnr.close();
 	}
-
+	// Method that is the "dice-rolling" method
 	public static int getDiceRoll(int diceSides) {
 		int diceVal = (int) (Math.random() * diceSides);
 		return diceVal + 1;
 	}
-
+	// Method that assigns a phrase based on result of rolling two 6-sided die
 	public static String getPhrase(int num1, int num2) {
 		String phrase;
 		if (num1 == 1 && num2 == 1) {
